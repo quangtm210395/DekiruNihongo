58324ccc8a082f5372020967708d7b12542e7595
phanmduong <phanminhduong@hotmail.com>
2016-03-27 11:37:39 +0700

    design màn hình chính + add methodsLesson

---
+++ 3.Project/DekiruNihongo/app/src/main/java/hmdq/js/codeproject/dekirunihongo/MethodsLesson.java
@@ -0,0 +1,27 @@
+package hmdq.js.codeproject.dekirunihongo;
+
+import android.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MethodsLesson extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // add actionbar;
+        setContentView(R.layout.activity_methods_lesson);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
+}
