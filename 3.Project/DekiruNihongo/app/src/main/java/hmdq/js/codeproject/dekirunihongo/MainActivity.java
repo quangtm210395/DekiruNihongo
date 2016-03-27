f5e81206818edaf74bad5fd7b40d5ba95e4393d5
phanmduong <phanminhduong@hotmail.com>
2016-03-27 12:07:25 +0700

    design màn hình chính + add methodsLesson

--- 3.Project/DekiruNihongo/app/src/main/java/hmdq/js/codeproject/dekirunihongo/MainActivity.java
+++ 3.Project/DekiruNihongo/app/src/main/java/hmdq/js/codeproject/dekirunihongo/MainActivity.java
@@ -2,12 +2,15 @@ package hmdq.js.codeproject.dekirunihongo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageButton iBtn1,iBtn2,iBtn3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 }
