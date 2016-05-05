5908cf5499c048d1fbe72dfb499672c271be020f
quangtm210395 <quangtm210395@users.noreply.github.com>
2016-05-05 11:04:36 +0700

    commit CommonData.java chưa hoàn thiện, k ảnh hưởng app

---
+++ 3.Project/DekiruNihongo/app/src/main/java/hmdq/js/codeproject/dekirunihongo/CommonData.java
@@ -0,0 +1,17 @@
+package hmdq.js.codeproject.dekirunihongo;
+
+/**
+ * Created by Minnn on 5/4/2016.
+ */
+public class CommonData {
+    public static String noLesson;
+    public static String noBook;
+    private static CommonData ourInstance = new CommonData();
+
+    public static CommonData getInstance() {
+        return ourInstance;
+    }
+
+    private CommonData() {
+    }
+}
